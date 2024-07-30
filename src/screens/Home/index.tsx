120026a0663983a0c8f1997e04283ea98cc15cf5
Anderson Lopes <developes.anderson@gmail.com>
2024-07-29 23:25:37 -0400

    ✨ Criando o form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,17 +11,19 @@ export default function Home(){
         <Text style={styles.eventName}>Nome do evento</Text>
         <Text style={styles.eventDate}>Segunda, 29 de julho de 2024.</Text>
         
-        <TextInput style={
-          styles.input}
-          placeholder="Nome do participante"
-          placeholderTextColor="#6b6b6b"
-          />
-          
-          <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-            <Text style={styles.buttonText}>
-              +
-            </Text>
-          </TouchableOpacity>
+        <View style={styles.form}>
+          <TextInput style={
+              styles.input}
+              placeholder="Nome do participante"
+              placeholderTextColor="#6b6b6b"
+              />
+              
+              <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                <Text style={styles.buttonText}>
+                  +
+                </Text>
+              </TouchableOpacity>
+        </View>
     </View>
   )
 }
