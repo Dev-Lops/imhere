7fc4c5cd750d7a7969e4d0c1293638c67819fd78
Anderson Lopes <developes.anderson@gmail.com>
2024-07-29 22:46:51 -0400

    ✨ Introdução ao stylesheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App(){
   return(
-    <View>
-      <>
-        <Text key="1">React</Text>
-        <Text key="2">Native</Text>
-      </>
+    <View style={styles.container}>
+        <Text style={styles.title}>Nome do evento</Text>
+        <Text style={styles.date}>Segunda, 29 de julho de 2024.</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  date: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})
